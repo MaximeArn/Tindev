891c187b3c0a2d9a1c72dd02754b81cd0ab35b32
Krysyx <anthololz31@yahoo.fr>
2020-10-01 23:51:06 +0200

    minor fix

--- src/selectors/inputType.tsx
+++ src/selectors/inputType.tsx
@@ -1,11 +1,8 @@
-const inputs: any = [
-  { age: "number" },
-  { email: "email" },
-  { password: "password" },
-  { confirmPassword: "password" },
-];
-
-export default (name: string) => {
-  const inputType = inputs.find((obj: any) => obj[name]);
-  return inputType ? inputType[name] : "text";
+const input: any = {
+  age: "number",
+  email: "email",
+  password: "password",
+  confirmPassword: "password",
 };
+
+export default (name: string) => input[name] || "text";
