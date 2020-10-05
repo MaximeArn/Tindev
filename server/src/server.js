f176688a1468e5dd9f90544e032211edb6b27f46
Krysyx <anthololz31@yahoo.fr>
2020-10-05 21:23:59 +0200

    404 middleware done

--- server/src/server.js
+++ server/src/server.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const server = require("express")();
 const authRouter = require("./router/auth");
-const { pageNotFound } = require("./middlewares/NotFound");
+const { pageNotFound: notFound } = require("./middlewares/NotFound");
 
 const PORT = process.env.PORT || 3000;
 
 server.use(express.json());
 server.use("/auth", authRouter);
-server.use(pageNotFound);
+server.use(notFound);
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
