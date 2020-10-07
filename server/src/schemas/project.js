1a0d4f3f8731fb878cb92572b8b8df52788b9a28
MaximeArn <maxime-arnould@outlook.fr>
2020-10-07 23:54:37 +0200

    disable versionKey

--- server/src/schemas/project.js
+++ server/src/schemas/project.js
@@ -1,12 +1,17 @@
+/** @format */
+
 const { Schema } = require("mongoose");
 
-const projectSchema = new Schema({
-  image: String,
-  author: String,
-  title: String,
-  description: String,
-  contributors: [{ name: String }],
-  avatar: String,
-});
+const projectSchema = new Schema(
+  {
+    image: String,
+    author: String,
+    title: String,
+    description: String,
+    contributors: [{ name: String }],
+    avatar: String,
+  },
+  { versionKey: false }
+);
 
 module.exports = projectSchema;
