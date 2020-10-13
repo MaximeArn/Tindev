6a80268c4c41d3cd228c7751989bdd23f00355e4
MaximeArn <maxime-arnould@outlook.fr>
2020-10-13 15:38:23 +0200

    mongo database script

---
+++ client/src/components/ProjectCreation/ProjectCreation.tsx
@@ -0,0 +1,90 @@
+/** @format */
+
+import React, { FormEvent, MouseEvent, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
+import { ProjectCreationProps } from "../../models/states";
+import capitalizeFirstLetter from "../../utils/capitalizeFirstLetter";
+import Buttons from "../containers/ProjectCreationButtons";
+import Input from "../containers/Input";
+import "./projectcreation.scss";
+
+const ProjectCreation = ({
+  projectInputs,
+  setProjectImage,
+  sendProject,
+}: ProjectCreationProps) => {
+  const fileInput = useRef<any>(null);
+
+  const onFileBrowserClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    fileInput.current.click();
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setProjectImage(fileInput.current.files[0]);
+    sendProject();
+  };
+
+  return (
+    <>
+      <input
+        name="image"
+        type="file"
+        ref={fileInput}
+        style={{ display: "none" }}
+      />
+      <div className="project-container">
+        <h2 className="project-creation-title-entry">Project Title : </h2>
+        <div className="project-creation">
+          <form onSubmit={handleSubmit}>
+            <Input
+              name="title"
+              placeHolder={capitalizeFirstLetter("title")}
+              inputValue={projectInputs["title"]}
+              formType="ProjectCreation"
+            />
+            <div className="separator"></div>
+
+            <h1 className="project-creation-title">
+              Project Description - try to be as detailed as possible :
+            </h1>
+
+            <Input
+              name="description"
+              inputValue={projectInputs["description"]}
+              formType="ProjectCreation"
+            />
+
+            <button
+              className="project-creation-button image"
+              onClick={onFileBrowserClick}
+            >
+              <FontAwesomeIcon icon={faPaperclip} />
+              Select An Image
+            </button>
+
+            <div className="separator"></div>
+
+            <h1 className="project-creation-title">Choose a category :</h1>
+
+            <Input name="category" formType="ProjectCreation" />
+
+            <div className="separator"></div>
+
+            <h1 className="project-creation-title">Desired team size :</h1>
+            <div className="project-creation-teamsize">
+              <Buttons />
+            </div>
+            <button type="submit" className="project-creation-button submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProjectCreation;
---
+++ server/mongo_script.js
@@ -0,0 +1,146 @@
+/** @format */
+
+let users = [
+  {
+    username: "Etienne",
+    firstname: "Etienne",
+    lastname: "JeSaisPas",
+    email: "Etienne@gmail.com",
+    password: "niquejason",
+    age: 30,
+    city: "Paris",
+    role: "user",
+  },
+  {
+    username: "Antho",
+    firstname: "Antho",
+    lastname: "",
+    email: "Antho@gmail.com",
+    password: "vivejason",
+    age: 26,
+    city: "lesud",
+    role: "user",
+  },
+  {
+    username: "Liv",
+    firstname: "Liv",
+    lastname: "Lacon",
+    email: "Liv@gmail.com",
+    password: "viveleslgbt",
+    age: 26,
+    city: "Brest",
+    role: "user",
+  },
+];
+
+let categories = [
+  {
+    name: "Video Game",
+    color: "orange",
+  },
+  {
+    name: "Web",
+    color: "purple",
+  },
+  {
+    name: "Software",
+    color: "green",
+  },
+  {
+    name: "AI",
+    color: "blue",
+  },
+  {
+    name: "Static",
+    color: "red",
+  },
+  {
+    name: "Java",
+    color: "purple",
+  },
+  {
+    name: "React",
+    color: "purple",
+  },
+  {
+    name: "Angular",
+    color: "purple",
+  },
+  {
+    name: "Vue",
+    color: "purple",
+  },
+  {
+    name: "NodeJS",
+    color: "purple",
+  },
+];
+
+let projects = [
+  {
+    image:
+      "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    author: "John",
+    title: "tindev",
+    description:
+      "Mon projet est claqué sa mère mais si il t'interesse tu peux quand même regarder de quoi ça parle",
+    contributors: [],
+    avatar:
+      "https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png",
+    categories: [],
+    size: 2,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    author: "John",
+    title: "tindev",
+    description:
+      "Mon projet est claqué sa mère mais si il t'interesse tu peux quand même regarder de quoi ça parle",
+    contributors: [],
+    avatar:
+      "https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png",
+    categories: [],
+    size: 3,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    author: "John",
+    title: "tindev",
+    description:
+      "Mon projet est claqué sa mère mais si il t'interesse tu peux quand même regarder de quoi ça parle",
+    contributors: [],
+    avatar:
+      "https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png",
+    categories: [],
+    size: 4,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    author: "John",
+    title: "tindev",
+    description:
+      "Mon projet est claqué sa mère mais si il t'interesse tu peux quand même regarder de quoi ça parle",
+    contributors: [],
+    avatar:
+      "https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png",
+    categories: [],
+    size: 5,
+  },
+];
+
+let collectionsInfos = [
+  { name: "users", data: users },
+  { name: "categories", data: categories },
+  { name: "projects", data: projects },
+];
+
+db.getCollectionNames().forEach((collection) => {
+  db[collection].drop();
+});
+
+collectionsInfos.forEach(({ name, data }) => {
+  db[name].insertMany(data);
+});
