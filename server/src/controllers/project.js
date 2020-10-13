8e6be4e77bd3140cc253dd502233acaada551af0
Krysyx <anthololz31@yahoo.fr>
2020-10-13 23:28:31 +0200

    tests to create new project in db : OK

--- server/src/controllers/project.js
+++ server/src/controllers/project.js
@@ -4,14 +4,9 @@ const fieldValidator = require("../utils/projectFieldValidator");
 module.exports = {
   create: async (req, res, next) => {
     let filename;
-    if (req.file) filename = req.file.filename;
+    req.file ? (filename = req.file) : (filename = null);
 
     const valid = await fieldValidator(req.body, next);
-
-    return valid && filename
-      ? Project.create({ ...valid, contributors: [], image: filename })
-      : valid
-      ? Project.create({ ...valid, contributors: [], image: null })
-      : null;
+    valid && Project.create({ ...valid, contributors: [], image: filename });
   },
 };
