46ac66db6b18f4797d3ea7aaefcf3440f7bc3cc8
Krysyx <anthololz31@yahoo.fr>
2020-10-18 19:51:46 +0200

    modal click handler done and working

--- client/src/components/ManageProject/DeclineModal.tsx
+++ client/src/components/ManageProject/DeclineModal.tsx
@@ -8,20 +8,35 @@ const DeclineModal = ({
   setModalStatus,
   declineApplicant,
 }: DeclineApplicantModalProps) => {
+  const handleConfirmClick = () => {
+    declineApplicant({ projectId, userId });
+    setModalStatus(false);
+  };
+
   return (
-    <div className="decline-applicant">
-      <div className="project-detail-padding">
-        <div className="decline-applicant-message">
-          Are you sure you want to decline this applicant ?
+    <div className="decline">
+      <div className="decline-applicant">
+        <div className="project-detail-padding">
+          <div className="decline-applicant-message">
+            Are you sure you want to decline this applicant ?
+          </div>
         </div>
-      </div>
-      <div className="decline-applicant-buttons">
-        <button className="decline-applicant-button" type="button">
-          Yes
-        </button>
-        <button className="decline-applicant-button" type="button">
-          No
-        </button>
+        <div className="decline-applicant-buttons">
+          <button
+            className="decline-applicant-button"
+            type="button"
+            onClick={handleConfirmClick}
+          >
+            Yes
+          </button>
+          <button
+            className="decline-applicant-button"
+            type="button"
+            onClick={() => setModalStatus(false)}
+          >
+            No
+          </button>
+        </div>
       </div>
     </div>
   );
