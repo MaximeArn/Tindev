4cc76b81e1ec9b69c1342e396dcd99df81527556
Krysyx <anthololz31@yahoo.fr>
2020-10-21 00:19:35 +0200

    fixing multer filefilter to do validator job before multer actually uploads the image

--- server/src/config/multer/fileFilter.js
+++ server/src/config/multer/fileFilter.js
@@ -1,12 +1,25 @@
 const ProjectError = require("../../utils/CustomError");
+const { Category } = require("../../models");
 const ALLOWED_MIME_TYPE = {
   "image/jpeg": "jpeg",
   "image/jpg": "jpg",
   "image/png": "png",
 };
 
-module.exports = ({ body }, { mimetype }, callback) => {
-  console.log(body);
+module.exports = async ({ body }, { mimetype }, callback) => {
+  const { title, description, size, categories: category } = body;
+
+  const validateFieldsValues = Object.values(body).every((value) => value);
+
+  !validateFieldsValues &&
+    callback(new ProjectError("Some required fields were not provided.", 400));
+
+  const categories = await Category.find();
+
+  !categories.some(({ name }) => name === category) &&
+    callback(new ProjectError("Invalid Category provided.", 400));
+
+  !description && callback(new ProjectError("Please fill in a description."));
   // ALLOWED_MIME_TYPE[mimetype]
   //   ? callback(null, true)
   //   : callback(new ProjectError("Invalid file format", 400));
