d183c488de66ee000456cb2dcc44464c048d28a4
Krysyx <anthololz31@yahoo.fr>
2020-10-22 23:30:09 +0200

    fixed reset inputs param types

--- client/src/utils/resetInputs.tsx
+++ client/src/utils/resetInputs.tsx
@@ -1,19 +1,15 @@
-import { ProjectCreation } from "../models/projects";
-
 const PROJECT_CREATION: any = {
   image: null,
   size: null,
   categories: [],
 };
 
-export default (inputs: any | ProjectCreation) => {
+export default (inputs: any) => {
   const inputsCopy = { ...inputs };
   for (const key in inputsCopy) {
     PROJECT_CREATION.hasOwnProperty(key)
       ? (inputsCopy[key] = PROJECT_CREATION[key])
       : (inputsCopy[key] = "");
   }
-
-  console.log("AFTER : ", inputsCopy);
   return inputsCopy;
 };
