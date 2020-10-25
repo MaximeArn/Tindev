a8e69456058309d98c2ba7188f6c413de932f326
Krysyx <anthololz31@yahoo.fr>
2020-10-25 02:25:55 +0200

    css lol

--- client/src/components/Users/UserProfile/User.tsx
+++ client/src/components/Users/UserProfile/User.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { User } from "../../../models/users";
+import { url } from "../../../environments/api";
 import "./userprofile.scss";
 
 const User = ({ username }: User) => {
   console.log(username);
-  return <div>HELLO HELLO HELLO</div>;
+  return (
+    <>
+      <div className="user-profile-preview">
+        <div className="user-profile-preview-header">
+          <img src={`${url}/uploads/users/default-image.jpg`} />
+        </div>
+
+        <div>
+          <p className="user-profile-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa
+            laborum necessitatibus harum sit voluptate architecto dicta, illo
+            eveniet perspiciatis reprehenderit, voluptatum nisi nulla animi a
+            voluptatem rem maiores voluptas vero!
+          </p>
+        </div>
+      </div>
+      {/* <div className="user-profile-introduce">
+        <p>AH OKI 2</p>
+      </div> */}
+    </>
+  );
 };
 
 export default User;
