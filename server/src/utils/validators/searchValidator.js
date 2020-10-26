c865937fe1e657206cd995fb208b50df2b3dc3b4
Krysyx <anthololz31@yahoo.fr>
2020-10-27 00:19:38 +0100

    fixed search validator regex

--- server/src/utils/validators/searchValidator.js
+++ server/src/utils/validators/searchValidator.js
@@ -3,27 +3,26 @@ const SearchError = require("../CustomError");
 module.exports = async ({ query }, next) => {
   try {
     if (!query) throw new SearchError("Invalid research", 400);
+    const regex = `^${query}`;
 
     const project = Project.find({
       $or: [
-        { title: { $regex: query, $options: "i" } },
-        { author: { $regex: query, $options: "i" } },
+        { title: { $regex: regex, $options: "i" } },
+        { author: { $regex: regex, $options: "i" } },
       ],
     });
 
     const user = User.find({
       $or: [
-        { username: { $regex: query, $options: "i" } },
-        { city: { $regex: query, $options: "i" } },
+        { username: { $regex: regex, $options: "i" } },
+        { city: { $regex: regex, $options: "i" } },
       ],
     });
 
     //not useful at the moment but might use it later on with a category allocated page
     const category = await Category.find({
-      name: { $regex: `(?i)${query}` },
+      name: { $regex: regex, $options: "i" },
     });
-
-    console.log(category);
 
     const [projects, users] = await Promise.all([project, user]);
 
