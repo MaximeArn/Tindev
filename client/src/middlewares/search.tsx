e6329636eeae14eaa78abded60bac4855fd1a0b9
Krysyx <anthololz31@yahoo.fr>
2020-10-27 16:29:33 +0100

    refactored search middleware to use a single method

--- client/src/middlewares/search.tsx
+++ client/src/middlewares/search.tsx
@@ -13,21 +13,12 @@ const sendSearch = ({ dispatch, getState, history }: AxiosSubmit) => {
     .get(`/search/${search}`)
     .then(({ data: results }) => {
       dispatch({ type: "SET_SEARCH_RESULTS", results });
-      history.push({
-        pathname: "/search",
-        search: `term=${search}`,
-      });
+      history &&
+        history.push({
+          pathname: "/search",
+          search: `term=${search}`,
+        });
     })
-    .catch(({ response }) => console.log(response));
-};
-
-const sendSearchPreview = ({ getState, dispatch }: AxiosSubmit) => {
-  const { search } = getState().search;
-  axios
-    .get(`/search/${search}`)
-    .then(({ data: results }) =>
-      dispatch({ type: "SET_SEARCH_RESULTS", results })
-    )
     .catch(({ response }) => console.log(response));
 };
 
@@ -35,10 +26,9 @@ const search: Middleware = ({ dispatch, getState }) => (next) => (action) => {
   const { history } = action;
   switch (action.type) {
     case "SEND_RESEARCH":
-      sendSearch({ dispatch, getState, history });
-      break;
-    case "SEND_SEARCH_PREVIEW":
-      sendSearchPreview({ dispatch, getState });
+      history
+        ? sendSearch({ dispatch, getState, history })
+        : sendSearch({ dispatch, getState });
       break;
     default:
       next(action);
