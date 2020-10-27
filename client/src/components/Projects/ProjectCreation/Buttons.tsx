8fcbd1c72a7e80089e8a20c39c5a788b421790d2
MaximeArn <maxime-arnould@outlook.fr>
2020-10-27 16:05:08 +0100

    merging team size input

--- client/src/components/Projects/ProjectCreation/Buttons.tsx
+++ client/src/components/Projects/ProjectCreation/Buttons.tsx
@@ -1,44 +1,65 @@
-import React, { MouseEvent } from "react";
+/** @format */
+
+import React, { MouseEvent, useState, useRef, ChangeEvent } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { ProjectCreationButton } from "../../../models/projects";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    lastButton: {
+      maxWidth: "50px",
+      padding: 0,
+    },
+  })
+);
 
 const Buttons = ({ getProjectTeamSize }: ProjectCreationButton) => {
   const handleButtonClick = ({
     currentTarget,
-  }: MouseEvent<HTMLButtonElement>) => {
+  }: MouseEvent<HTMLButtonElement> | ChangeEvent<HTMLInputElement>) => {
     getProjectTeamSize(currentTarget.value);
   };
+  const input = useRef<HTMLInputElement>(null);
+  const [lastButtonInput, setLastButtonInput] = useState(false);
+  const classes = useStyles();
+
   return (
-    <>
-      <button
-        type="button"
-        className="project-creation-button"
-        value={2}
-        onClick={handleButtonClick}
+    <ButtonGroup variant="contained" size="large">
+      <Button value={2} onClick={handleButtonClick}>
+        2
+      </Button>
+      <Button value={3} onClick={handleButtonClick}>
+        3
+      </Button>
+      <Button value={4} onClick={handleButtonClick}>
+        4
+      </Button>
+      <Button
+        onClick={() => {
+          setLastButtonInput(true);
+        }}
+        className={classes.lastButton}
+        value={input.current?.value}
       >
-        2 people
-      </button>
-      <button
-        type="button"
-        value={3}
-        className="project-creation-button"
-        onClick={handleButtonClick}
-      >
-        3 people
-      </button>
-      <button
-        type="button"
-        className="project-creation-button"
-        value={4}
-        onClick={handleButtonClick}
-      >
-        4 people
-      </button>
-      <button type="button" className="project-creation-button">
-        <FontAwesomeIcon icon={faPlus} color="grey" />
-      </button>
-    </>
+        {lastButtonInput ? (
+          <input
+            ref={input}
+            type="number"
+            onChange={handleButtonClick}
+            className="teamSize-Input"
+            defaultValue={5}
+            min={5}
+          />
+        ) : (
+          <FontAwesomeIcon icon={faPlus} color="grey" />
+        )}
+      </Button>
+    </ButtonGroup>
   );
 };
 
