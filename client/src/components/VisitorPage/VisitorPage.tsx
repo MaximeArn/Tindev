5cf9008508b7ef66ef7652bb7e852f729f832b49
MaximeArn <maxime-arnould@outlook.fr>
2020-10-27 22:45:04 +0100

    visitor page style

--- client/src/components/VisitorPage/VisitorPage.tsx
+++ client/src/components/VisitorPage/VisitorPage.tsx
@@ -2,13 +2,21 @@
 
 import React from "react";
 import "./visitorPage.scss";
-import { assets } from "../../../config/paths";
 import image from "src/assets/home-image.jpg";
 
 const VisitorPage = () => {
   return (
-    <div className="visitor-page">
-      <img className="background-image" src={image} alt="oui" />;
+    <div className="visitor-page" style={{ backgroundImage: `url(${image})` }}>
+      <div className="text">
+        <h2 className="title">welcome to tindev</h2>
+        <p className="welcome-text">
+          Réseau social permettant de reunir des devs autour d'un projet commun.
+          Vous pourrez proposer un projet ou vous joindre au projet d'un autre
+          utilisateur. Vous aurez la possibilité de filtrer les recherches selon
+          vos critères. Discutez avec les autres utilisateurs via une messagerie
+          instantanée et créez des groupes afin de faciliter la communication.
+        </p>
+      </div>
     </div>
   );
 };
