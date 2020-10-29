81109093cd36782d75896dc57f202c9ba1a9eed1
Krysyx <anthololz31@yahoo.fr>
2020-10-29 22:06:10 +0100

    patch file filter multer middleware done and working

--- server/src/config/multer/patchFileFilter.js
+++ server/src/config/multer/patchFileFilter.js
@@ -1,7 +1,41 @@
+const { tokenValidator } = require("../../utils/validators");
+const { Project } = require("../../models");
+const ProjectError = require("../../utils/CustomError");
+const fs = require("fs").promises;
+const pathResolver = require("path");
+const ALLOWED_MIME_TYPE = {
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+  "image/png": "png",
+};
+
 module.exports = async (
-  { body, cookies: { token } },
+  { params: { id }, cookies: { token } },
   { mimetype },
   callback
 ) => {
-  console.log("NEW FILE FILTER");
+  try {
+    const user = await tokenValidator(token, null);
+    const project = await Project.findOne({ _id: id });
+    const path = pathResolver.join(__dirname, "../../public/uploads");
+
+    if (!user) {
+      return callback(new ProjectError("User not found.", 404));
+    }
+
+    if (!project) {
+      return callback(new ProjectError("This project does not exist.", 404));
+    }
+
+    if (!ALLOWED_MIME_TYPE[mimetype]) {
+      return callback(new ProjectError("Invalid file format provided", 400));
+    }
+
+    project.image !== "image-default.jpeg" &&
+      (await fs.unlink(`${path}/${project.image}`));
+
+    return callback(null, true);
+  } catch (error) {
+    console.error(error);
+  }
 };
