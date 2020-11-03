582f325e88e6782d42e1eb2589b31e06fc37c9e4
MaximeArn <maxime-arnould@outlook.fr>
2020-11-03 20:24:03 +0100

    prepare delete chat window button

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,29 +1,43 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import CloseIcon from "@material-ui/icons/Close";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 import "./chat.scss";
 
 const Chat = () => {
   const [chat, setChat] = useState(false);
+  const chatHeader = useRef(null);
   return (
     <div className="chatZone">
       {chat ? (
         <>
-          <div className="chatZone-header">
+          <div className="chatZone-header" onClick={() => setChat(!chat)}>
             <p>Michel</p>
-            <button className="closeIcon" onClick={() => setChat(!chat)}>
+            <button className="closeIcon">
               <CloseIcon />
             </button>
           </div>
           <div className="chatZone-content"></div>
         </>
       ) : (
-        <div className="chatZone-header">
+        <div
+          ref={chatHeader}
+          className="chatZone-header"
+          onClick={(event) => {
+            event.target === chatHeader.current && setChat(!chat);
+            console.log("element clicked : ", event.target);
+            console.log("close button : ", chatHeader.current);
+          }}
+        >
           <p>Michel</p>
-          <button className="closeIcon" onClick={() => setChat(!chat)}>
-            <ExpandLessIcon />
+          <button
+            className="closeIcon"
+            onClick={() => {
+              // console.log("destroy");
+            }}
+          >
+            <CloseIcon />
           </button>
         </div>
       )}
