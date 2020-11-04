cd63ada74fa896649ca82db91bef3423597d9681
Krysyx <anthololz31@yahoo.fr>
2020-11-04 14:37:05 +0100

    added date to socket emition

--- server/src/controllers/chat.js
+++ server/src/controllers/chat.js
@@ -6,13 +6,13 @@ module.exports = {
     socket.on("chat-message", async ({ to, message, token }) => {
       try {
         const { id } = await tokenValidator(token, null);
-
+        const date = Date.now();
         const user = await User.findOne({ _id: id });
-        user.messages.push({ to, message, date: Date.now() });
+        user.messages.push({ to, message, date });
         await user.save();
 
         [socket, connectedUsers[to.name]].forEach((socket) =>
-          socket.emit("chat-message", message)
+          socket.emit("chat-message", { message, date })
         );
       } catch (error) {
         throw new Error(error);
