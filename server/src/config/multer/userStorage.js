c09179d7242de67f43c4b75d75595d660ca878e8
Krysyx <anthololz31@yahoo.fr>
2020-11-16 05:07:24 +0100

    finished userStorage config

--- server/src/config/multer/userStorage.js
+++ server/src/config/multer/userStorage.js
@@ -10,5 +10,6 @@ const extensions = {
 module.exports = multer.diskStorage({
   destination: (req, file, callback) =>
     callback(null, path.join(__dirname, "../../public/uploads/users")),
-  filename: (req, { mimetype }, callback) => {},
+  filename: (req, { mimetype }, callback) =>
+    callback(null, `avatar-${Date.now()}.${extensions[mimetype]}`),
 });
