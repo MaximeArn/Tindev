2b968f877b2e45d0891aca6feae6cc0ac6cae455
Krysyx <anthololz31@yahoo.fr>
2020-11-18 06:06:59 +0100

    fixed avatar to correctly display on project edit closed

--- client/src/components/Users/EditProfile/ProfileEditClosed.tsx
+++ client/src/components/Users/EditProfile/ProfileEditClosed.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { UserProfileClosed } from "../../../models/users";
 import capitalize from "../../../utils/capitalizeFirstLetter";
+const { url } = require("../../../environments/api");
 
 const ProfileEditClosed = ({
   name,
@@ -11,7 +12,15 @@ const ProfileEditClosed = ({
     <>
       <div className="profile-field">
         <div className="field-name">{capitalize(name)} :</div>
-        <div className="field-value">{value}</div>
+        {name === "avatar" ? (
+          <img
+            className="profile-avatar"
+            src={`${url}/uploads/users/${value}`}
+            alt="user-avatar"
+          />
+        ) : (
+          <div className="field-value">{value}</div>
+        )}
       </div>
       <button className="field-modify" onClick={() => setEditStatus(true)}>
         Modify
