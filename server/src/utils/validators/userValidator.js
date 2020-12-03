877ef3306578082cefaf62cab5208d2415639c71
Krysyx <anthololz31@yahoo.fr>
2020-12-03 22:33:33 +0100

    fixed server side get user by username router and controller

--- server/src/utils/validators/userValidator.js
+++ server/src/utils/validators/userValidator.js
@@ -3,7 +3,10 @@ const UserError = require("../CustomError");
 
 module.exports = async (username, next) => {
   try {
-    const user = await User.findOne({ username });
+    const user = await User.findOne(
+      { username },
+      { password: 0, messages: 0, notifications: 0 }
+    );
     if (!user) throw new UserError("This user does not exist.", 404);
 
     return user;
