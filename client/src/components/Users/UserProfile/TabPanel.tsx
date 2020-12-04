17da389179ea3b930915ac0d6cf4c15d1586041b
Krysyx <anthololz31@yahoo.fr>
2020-12-04 03:31:12 +0100

    fixed default message condition in tab panel component

--- client/src/components/Users/UserProfile/TabPanel.tsx
+++ client/src/components/Users/UserProfile/TabPanel.tsx
@@ -5,14 +5,18 @@ const TabPanel = ({ content: { value } }: UserTabPanelProps) => {
   //TODO: make condition check to verify if value is null or, if it is an array , check if the array length is positive , otherwise display default information private message with lock icon
   return (
     <>
-      {Array.isArray(value) ? (
+      {Array.isArray(value) && value.length ? (
         <ul className="user-profile-content-list">
           {value.map((val: string) => (
             <li key={val}>{val}</li>
           ))}
         </ul>
       ) : (
-        <div className="user-profile-content-list">{value}</div>
+        <div className="user-profile-content-list">
+          {!value || Array.isArray(value)
+            ? "This information is private"
+            : value}
+        </div>
       )}
     </>
   );
