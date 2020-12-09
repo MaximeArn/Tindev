e3201cb8cf8c8c4ec8cd40c3ba136041b1146e2e
Krysyx <anthololz31@yahoo.fr>
2020-12-09 15:06:24 +0100

    starting chat message refactoring

--- server/src/server.js
+++ server/src/server.js
@@ -20,6 +20,8 @@ const {
   notificationsRouter,
 } = require("./router");
 const { name } = require("./config/database");
+const { nextTick } = require("process");
+const User = require("./models/User");
 
 const PORT = process.env.PORT || 7000;
 const SOCKET = process.env.SOCKET || 6000;
