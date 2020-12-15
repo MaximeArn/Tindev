8fdceb9d2d34276ec80886597bd757d4fe1e1233
Krysyx <anthololz31@yahoo.fr>
2020-12-15 03:02:44 +0100

    fixed chat component undefined on messages state mapping

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -10,30 +10,36 @@ const Chat = ({
   sendMessage,
   closeChatWindow,
 }: ChatProps) => {
+  const receivedMessages = messages[username] || [];
+
   return (
     <>
       <div className="chat-window-wrapper">
         {chatWindows.length > 0 &&
-          chatWindows.map((window: any) => (
-            <ChatWindows
-              key={window.username}
-              {...window}
-              messages={messages[window.username]
-                .concat(
-                  messages[username].filter(
-                    ({ from }: SocketServerResponse) => from === window.username
+          chatWindows.map((window: any) => {
+            const sentMessages = messages[window.username] || [];
+            return (
+              <ChatWindows
+                key={window.username}
+                {...window}
+                messages={sentMessages
+                  .concat(
+                    receivedMessages.filter(
+                      ({ from }: SocketServerResponse) =>
+                        from === window.username
+                    )
                   )
-                )
-                .sort(
-                  (
-                    { date: date1 }: SocketServerResponse,
-                    { date: date2 }: SocketServerResponse
-                  ) => (date1 > date2 ? -1 : 1)
-                )}
-              sendMessage={sendMessage}
-              closeChatWindow={closeChatWindow}
-            />
-          ))}
+                  .sort(
+                    (
+                      { date: date1 }: SocketServerResponse,
+                      { date: date2 }: SocketServerResponse
+                    ) => (date1 > date2 ? -1 : 1)
+                  )}
+                sendMessage={sendMessage}
+                closeChatWindow={closeChatWindow}
+              />
+            );
+          })}
       </div>
     </>
   );
