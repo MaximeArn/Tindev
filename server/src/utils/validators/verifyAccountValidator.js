42f33427d0c52ff35f345e52f7dc059425a9feff
Krysyx <anthololz31@yahoo.fr>
2020-12-18 09:05:07 +0100

    fixed verify account validator to correctly remove token after validation

--- server/src/utils/validators/verifyAccountValidator.js
+++ server/src/utils/validators/verifyAccountValidator.js
@@ -3,13 +3,17 @@ const AccountError = require("../CustomError");
 
 module.exports = async (token, next) => {
   try {
-    const { userId } = await Token.findOne({ token });
+    const token = await Token.findOne({ token });
 
-    if (!userId) {
+    if (!token.userId) {
       throw new AccountError("This Activation link is not valid anymore.", 400);
     }
 
-    return await User.findById(userId);
+    const user = await User.findById(userId);
+
+    await token.remove();
+
+    return user;
   } catch (error) {
     next(error);
   }
