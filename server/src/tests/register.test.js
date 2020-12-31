10f56f274a941b2cfbe622d7bbefbd2efc768c9c
Krysyx <anthololz31@yahoo.fr>
2020-12-31 14:02:09 +0100

    adjusted register test function

--- server/src/tests/register.test.js
+++ server/src/tests/register.test.js
@@ -7,19 +7,23 @@ describe("create user endpoint", () => {
   afterAll(() => disconnect());
 
   it("should return a 200 status with a json success message", async (done) => {
-    const { status, body } = await request.post("/auth/register").send({
-      username: "User1",
-      email: "user1@gmail.com",
-      password: "user1",
-      confirmPassword: "user1",
-    });
+    try {
+      const { status, body } = await request.post("/auth/register").send({
+        username: "User1",
+        email: "user1@gmail.com",
+        password: "user1",
+        confirmPassword: "user1",
+      });
 
-    expect(status).toEqual(200);
-    expect(body).toHaveProperty(
-      "msg",
-      "An email has been sent to your email address"
-    );
+      expect(status).toEqual(200);
+      expect(body).toHaveProperty(
+        "msg",
+        "An email has been sent to your email address"
+      );
 
-    done();
+      done();
+    } catch (error) {
+      done(error);
+    }
   });
 });
