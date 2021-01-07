4eab64f5944d26604318ada94851663147f75c64
Krysyx <anthololz31@yahoo.fr>
2021-01-07 16:52:05 +0100

    fixed child key in reset password component

--- client/src/components/ResetPassword/ResetPassword.tsx
+++ client/src/components/ResetPassword/ResetPassword.tsx
@@ -4,13 +4,13 @@ import Input from "../containers/Input";
 import "./resetpassword.scss";
 
 const ResetPassword = ({ inputs }: ResetPasswordProps) => {
-  console.log(inputs);
   return (
     <div className="reset-password">
       <form action="" className="reset-password-form">
         {Object.entries(inputs).map(([key, value]) => {
           return (
             <Input
+              key={key}
               name={key}
               inputValue={value}
               required={true}
