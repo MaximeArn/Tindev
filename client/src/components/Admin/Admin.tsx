d535d7848fead873ca771a593afb2ef4d25b89dc
MaximeArn <maxime-arnould@outlook.fr>
2021-01-10 14:33:00 +0100

    open admin panel animation

--- client/src/components/Admin/Admin.tsx
+++ client/src/components/Admin/Admin.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrash,
+  faChevronRight,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
 import "./admin.scss";
 import { AdminOverlayProps } from "../../models/states";
 
 const Admin = ({ id, collection }: AdminOverlayProps) => {
+  const [isPanelOpen, setIsPanelOpen] = useState(false);
+
   return (
-    <div className="admin-overlay">
-      <button onClick={() => console.log("delete !!")}>
-        <FontAwesomeIcon icon={faTrash} />
-      </button>
+    <div className={isPanelOpen ? "admin-overlay open" : "admin-overlay"}>
+      <div className="admin-overlay-content">
+        <h3>Admin Panel</h3>
+        <button onClick={() => console.log("delete !!")}>
+          <FontAwesomeIcon icon={faTrash} color="red" />
+        </button>
+        {isPanelOpen && (
+          <div className="arrow-close">
+            <button onClick={() => setIsPanelOpen(!isPanelOpen)}>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </button>
+          </div>
+        )}
+      </div>
+      {!isPanelOpen && (
+        <div>
+          <button onClick={() => setIsPanelOpen(!isPanelOpen)}>
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
