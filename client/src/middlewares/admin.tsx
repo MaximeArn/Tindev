07651689d449e756324e991c9b1586b9cf15e027
MaximeArn <maxime-arnould@outlook.fr>
2021-01-10 14:56:52 +0100

    amin middleware delete project method

--- client/src/middlewares/admin.tsx
+++ client/src/middlewares/admin.tsx
@@ -1,8 +1,19 @@
 import { Middleware } from "redux";
+import axios from "axios";
+import { url } from "../environments/api";
+axios.defaults.baseURL = url;
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.withCredentials = true;
 
 const admin: Middleware = ({ getState, dispatch }) => (next) => (action) => {
-  const { type } = action;
+  const { type, id } = action;
   switch (type) {
+    case "DELETE_PROJECT":
+      axios
+        .delete(`/admin/project/${id}`)
+        .then(({ data }) => console.log(data))
+        .catch(({ response: { data: { msg } } }) => console.log(msg));
+      break;
     default:
       next(action);
       break;
