887e76d1ddd591b1b0e6764dea6cdb6b552d6640
MaximeArn <maxime-arnould@outlook.fr>
2021-01-19 21:05:31 +0100

    change capitalize function to accept more than 1 word

--- client/src/utils/capitalizeFirstLetter.tsx
+++ client/src/utils/capitalizeFirstLetter.tsx
@@ -1,1 +1,9 @@
-export default (word: string) => `${word[0].toUpperCase()}${word.substring(1)}`;
+// export default (word: string) => `${word[0].toUpperCase()}${word.substring(1)}`
+
+export default (string: string) =>
+  String(
+    string
+      .split(" ")
+      .map((word: string) => `${word[0].toUpperCase()}${word.substring(1)}`)
+      .join(" ")
+  );
