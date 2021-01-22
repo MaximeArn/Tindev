b4fcb7e24af4e5a4bd0e2c955acab766e3327061
MaximeArn <maxime-arnould@outlook.fr>
2021-01-22 16:14:44 +0100

    change shortifyString test

--- client/src/tests/shortifyString.test.ts
+++ client/src/tests/shortifyString.test.ts
@@ -1,19 +1,5 @@
 import shortifyString from "../utils/shortifyString";
 
-// test("should return a string shorter than 180 characters", () => {
-//   expect(shortifyString("a short string")).toBe("a short string");
-
-//   expect(
-//     shortifyString(
-//       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque veniam harum sequi, quidem, nesciunt debitis facere laborum beatae iste consectetur officia laboriosam quia mollitia accusamus nisi vitae ipsa eligendi asperiores. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque veniam harum sequi, quidem, nesciunt debitis facere laborum beatae iste consectetur officia laboriosam quia mollitia accusamus nisi vitae ipsa eligendi asperiores."
-//     )
-//   ).toHaveLength(180);
-
-//   expect(typeof shortifyString("a string")).toBe("string");
-
-//   expect(shortifyString("a string")).toBeTruthy;
-// });
-
 describe("shortifyString function", () => {
   it("should return same string if short", () => {
     expect(shortifyString("a short string")).toBe("a short string");
