1d5257b1c7a6f229385c89e546be0d8197489391
MaximeArn <maxime-arnould@outlook.fr>
2021-01-22 15:39:32 +0100

    add test for shortifyString

--- client/src/tests/shortifyString.test.ts
+++ client/src/tests/shortifyString.test.ts
@@ -1,11 +1,37 @@
 import shortifyString from "../utils/shortifyString";
 
-test("should return a string shorter than 180 characters", () => {
-  expect(shortifyString("a short string")).toBe("a short string");
+// test("should return a string shorter than 180 characters", () => {
+//   expect(shortifyString("a short string")).toBe("a short string");
 
-  expect(
-    shortifyString(
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque veniam harum sequi, quidem, nesciunt debitis facere laborum beatae iste consectetur officia laboriosam quia mollitia accusamus nisi vitae ipsa eligendi asperiores. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque veniam harum sequi, quidem, nesciunt debitis facere laborum beatae iste consectetur officia laboriosam quia mollitia accusamus nisi vitae ipsa eligendi asperiores."
-    )
-  ).toHaveLength(180);
+//   expect(
+//     shortifyString(
+//       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque veniam harum sequi, quidem, nesciunt debitis facere laborum beatae iste consectetur officia laboriosam quia mollitia accusamus nisi vitae ipsa eligendi asperiores. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque veniam harum sequi, quidem, nesciunt debitis facere laborum beatae iste consectetur officia laboriosam quia mollitia accusamus nisi vitae ipsa eligendi asperiores."
+//     )
+//   ).toHaveLength(180);
+
+//   expect(typeof shortifyString("a string")).toBe("string");
+
+//   expect(shortifyString("a string")).toBeTruthy;
+// });
+
+describe("shortifyString function", () => {
+  it("should return same string if short", () => {
+    expect(shortifyString("a short string")).toBe("a short string");
+  });
+
+  it("should return a shortified string if long", () => {
+    expect(
+      shortifyString(
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque veniam harum sequi, quidem, nesciunt debitis facere laborum beatae iste consectetur officia laboriosam quia mollitia accusamus nisi vitae ipsa eligendi asperiores. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque veniam harum sequi, quidem, nesciunt debitis facere laborum beatae iste consectetur officia laboriosam quia mollitia accusamus nisi vitae ipsa eligendi asperiores."
+      )
+    ).toHaveLength(180);
+  });
+
+  it("should return a strin", () => {
+    expect(typeof shortifyString("a string")).toBe("string");
+  });
+
+  it("should return a truthy value", () => {
+    expect(shortifyString("a string")).toBeTruthy;
+  });
 });
