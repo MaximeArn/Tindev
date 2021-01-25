edce43caedf3535818494ad27347403fea5d00fa
MaximeArn <maxime-arnould@outlook.fr>
2021-01-25 22:58:49 +0100

    create db seed script

--- server/mongo_script.js
+++ server/mongo_script.js
@@ -1,130 +1,141 @@
-let users = [
-  {
-    username: "Etienne",
-    firstname: "Etienne",
-    lastname: "JeSaisPas",
-    email: "Etienne@gmail.com",
-    password: "niquejason",
-    age: 30,
-    city: "Paris",
-    role: "user",
-  },
-  {
-    username: "Antho",
-    firstname: "Antho",
-    lastname: "Jesaispas2",
-    email: "Antho@gmail.com",
-    password: "vivejason",
-    age: 26,
-    city: "lesud",
-    role: "user",
-  },
-  {
-    username: "Liv",
-    firstname: "Liv",
-    lastname: "Jesaispas3",
-    email: "Liv@gmail.com",
-    password: "viveleslgbt",
-    age: 26,
-    city: "Brest",
-    role: "user",
-  },
-];
+require("dotenv").config();
+const hashPassword = require("./src/utils/hashPassword");
+const { User, Project, Category } = require("./src/models");
+const connection = require("./src/config/database");
 
-let categories = [
-  {
-    name: "Video Game",
-  },
-  {
-    name: "Web",
-  },
-  {
-    name: "Software",
-  },
-  {
-    name: "AI",
-  },
-  {
-    name: "Static",
-  },
-  {
-    name: "Java",
-  },
-  {
-    name: "React",
-  },
-  {
-    name: "Angular",
-  },
-  {
-    name: "Vue",
-  },
-  {
-    name: "NodeJS",
-  },
-];
+(async () => {
+  connection.dropDatabase();
 
-let projects = [
-  {
-    image: "image-default.jpeg",
-    author: "John",
-    title: "Pomodoro",
-    description: `Le Pomodoro, pour ceux qui ne connaissent pas, c'est quoi ?
-      C'est une technique de gestion du temps, basée sur un minuteur. On sélectionne une tâche, puis on travaille sur des durées de 25 minutes, et on enchaîne sur de courtes pauses, par cycle de 4 pour arriver à une pause plus longue.`,
-    contributors: [],
-    categories: ["Java", "Software", "AI", "Angular"],
-    applicants: [],
-    size: 2,
-  },
-  {
-    image: "image-default.jpeg",
-    author: "John",
-    title: "Voyage Voyage",
-    description: `Un outil qui permettrait à chaque personne ayant l’âme d'un leader de groupe d'organiser un voyage que ce soit pour un séminaire professionnel, un voyage entre amis ou en famille ou autre.`,
-    contributors: [],
-    categories: ["Java", "Software", "AI", "Angular"],
-    applicants: [],
-    size: 3,
-  },
-  {
-    image: "image-default.jpeg",
-    author: "John",
-    title: "Jardins Connectés",
-    description: `S'entraider entre jardiniers pour être plus autonomes dans nos productions au potager.
-      Gérer des données sur notre propre potager pour les analyser et s'améliorer.
-      Voir les jardiniers qui ont de bons résultats dans son secteur pour s'entraider.
-      S'appuyer sur un référentiel de fiches diverses en BDD pour mieux gérer nos potagers.
-      Le but est de faire un projet qui nous plait, de continuer à apprendre et de travailler en équipe. C'est certainement un poil ambitieux, alors on peut évidemment simplifier.`,
-    contributors: [],
-    categories: ["Java", "Software", "AI", "Angular"],
-    applicants: [],
-    size: 4,
-  },
-  {
-    image: "image-default.jpeg",
-    author: "John",
-    title: "tindev",
-    description: `Réseau social permettant de réunir des devs autour d'un projet commun.
-      Vous pourrez proposer un projet ou vous joindre au projet d'un autre utilisateur.
-      Vous aurez la possibilité de filtrer les recherches selon vos critères.
-      Discutez avec les autres utilisateurs via une messagerie instantanée et créez des groupes afin de faciliter la communication.`,
-    contributors: [],
-    categories: ["Java", "Software", "AI", "Angular"],
-    applicants: [],
-    size: 5,
-  },
-];
+  const users = [
+    {
+      username: "etienne",
+      email: "etienne@gmail.com",
+      password: await hashPassword("etienne"),
+    },
+    {
+      username: "charles",
+      email: "charles@gmail.com",
+      password: await hashPassword("charles"),
+    },
+    {
+      username: "antho",
+      email: "antho@gmail.com",
+      password: await hashPassword("antho"),
+    },
+    {
+      username: "maxime",
+      email: "maxime@gmail.com",
+      password: await hashPassword("maxime"),
+    },
+    {
+      username: "admin",
+      email: "admin@gmail.com",
+      password: await hashPassword("admin"),
+      role: "Admin",
+    },
+    {
+      username: "admin2",
+      email: "admin2@gmail.com",
+      password: await hashPassword("admin2"),
+      role: "Admin",
+    },
+    {
+      username: "tanguy",
+      email: "tanguy@gmail.com",
+      password: await hashPassword("tanguy"),
+    },
+    {
+      username: "john",
+      email: "john@gmail.com",
+      password: await hashPassword("john"),
+    },
+  ];
 
-let collectionsInfos = [
-  { name: "users", data: users },
-  { name: "categories", data: categories },
-  { name: "projects", data: projects },
-];
+  const categories = [
+    {
+      name: "Video Game",
+    },
+    {
+      name: "Web",
+    },
+    {
+      name: "Software",
+    },
+    {
+      name: "AI",
+    },
+    {
+      name: "Java",
+    },
+    {
+      name: "React",
+    },
+    {
+      name: "Angular",
+    },
+    {
+      name: "Vue",
+    },
+    {
+      name: "NodeJS",
+    },
+    {
+      name: "PHP",
+    },
+    {
+      name: "Python",
+    },
+    {
+      name: "Rust",
+    },
+    {
+      name: "Go",
+    },
+    {
+      name: "Swift",
+    },
+  ];
 
-db.getCollectionNames().forEach((collection) => {
-  db[collection].drop();
-});
+  const projects = [
+    {
+      author: "John",
+      title: "Pomodoro",
+      description: `Le Pomodoro, pour ceux qui ne connaissent pas, c'est quoi ?
+        C'est une technique de gestion du temps, basée sur un minuteur. On sélectionne une tâche, puis on travaille sur des durées de 25 minutes, et on enchaîne sur de courtes pauses, par cycle de 4 pour arriver à une pause plus longue.`,
+      categories: ["Java", "Software", "AI", "Angular"],
+      size: 2,
+    },
+    {
+      author: "John",
+      title: "Voyage Voyage",
+      description: `Un outil qui permettrait à chaque personne ayant l’âme d'un leader de groupe d'organiser un voyage que ce soit pour un séminaire professionnel, un voyage entre amis ou en famille ou autre.`,
+      categories: ["Java", "Software", "AI", "Angular"],
+      size: 3,
+    },
+    {
+      author: "John",
+      title: "Jardins Connectés",
+      description: `S'entraider entre jardiniers pour être plus autonomes dans nos productions au potager.
+        Gérer des données sur notre propre potager pour les analyser et s'améliorer.
+        Voir les jardiniers qui ont de bons résultats dans son secteur pour s'entraider.
+        S'appuyer sur un référentiel de fiches diverses en BDD pour mieux gérer nos potagers.
+        Le but est de faire un projet qui nous plait, de continuer à apprendre et de travailler en équipe. C'est certainement un poil ambitieux, alors on peut évidemment simplifier.`,
+      categories: ["Java", "Software", "AI", "Angular"],
+      size: 4,
+    },
+    {
+      author: "John",
+      title: "tindev",
+      description: `Réseau social permettant de réunir des devs autour d'un projet commun.
+        Vous pourrez proposer un projet ou vous joindre au projet d'un autre utilisateur.
+        Vous aurez la possibilité de filtrer les recherches selon vos critères.
+        Discutez avec les autres utilisateurs via une messagerie instantanée et créez des groupes afin de faciliter la communication.`,
+      categories: ["Java", "Software", "AI", "Angular"],
+      size: 5,
+    },
+  ];
 
-collectionsInfos.forEach(({ name, data }) => {
-  db[name].insertMany(data);
-});
+  users.forEach(async (user) => await User.create(user));
+  projects.forEach(async (project) => await Project.create(project));
+  categories.forEach(async (category) => await Category.create(category));
+})();
