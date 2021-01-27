9ec89e3272d3564ad76bc2e4e76795b786c82875
Krysyx <anthololz31@yahoo.fr>
2021-01-27 14:54:45 +0100

    deleted useless message schema

--- server/src/schemas/message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-
-const messageSchema = new Schema({
-  author: String,
-  content: String,
-});
-
-module.exports = messageSchema;
