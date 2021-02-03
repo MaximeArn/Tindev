09ec6b8bbb544b83ea8524a6ac07647f64460251
MaximeArn <maxime-arnould@outlook.fr>
2021-02-03 11:41:39 +0100

    remove reset from  scrollreveal options

--- client/src/utils/scrollReveal.ts
+++ client/src/utils/scrollReveal.ts
@@ -1,6 +1,6 @@
-import ScrollReveal from 'scrollreveal';
+import ScrollReveal from "scrollreveal";
 export const cardOptions = {
-    delay:100,
-    reset: true
+  delay: 100,
+  reset: false,
 };
 export default ScrollReveal();
