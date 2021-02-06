4f2ba972e334817801849a8d98ab30998e0d6262
Krysyx <anthololz31@yahoo.fr>
2021-02-06 14:21:08 +0100

    added modal click handler inside project deletion modal component

--- client/src/components/Projects/EditProject/Modals/DeletionModal.tsx
+++ client/src/components/Projects/EditProject/Modals/DeletionModal.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { DeleteProjectModalProps } from "../../../../models/projects";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { useHistory } from "react-router-dom";
@@ -10,69 +10,78 @@ const Modal = ({
   loader,
   success,
 }: DeleteProjectModalProps) => {
-  const modalContainer = useRef<HTMLDivElement>(null);
+  const modalContainer = useRef<any>(null);
   const history = useHistory();
+  console.log(success);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, []);
+
+  const handleClick = ({ target }: any) => {
+    console.log({ target, contains: modalContainer.current.contains(target) });
+
+    !modalContainer.current.contains(target) &&
+      !(target?.className === "decline-applicant-button") &&
+      setModalStatus(false);
+  };
 
   return (
     <>
-      <div
-        ref={modalContainer}
-        onClick={({ target }) => {
-          target === modalContainer.current && setModalStatus(false);
-          success && history.push("/");
-        }}
-        className="decline"
-      >
-        {success ? (
-          <div className="apply-success">
-            <div className="project-detail-padding">
-              <div className="apply-success-message">{success}</div>
-            </div>
-            <button
-              onClick={() => {
-                setModalStatus(false);
-                history.push("/");
-              }}
-              className="apply-success-button"
-              type="button"
-            >
-              Close
-            </button>
-          </div>
-        ) : (
-          <div className="decline-applicant">
-            <div className="project-detail-padding">
-              <div className="decline-applicant-message">
-                Are you sure to delete this project ?
+      <div className="decline">
+        <div ref={modalContainer} className="decline-applicant">
+          {success ? (
+            <>
+              <div className="project-detail-padding">
+                <div className="apply-success-message">{success}</div>
               </div>
-            </div>
-            <div className="decline-applicant-buttons">
-              {loader ? (
-                <div className="project-deletion-loader">
-                  <p>Loading</p>
-                  <CircularProgress size={15} />
+              <button
+                onClick={() => {
+                  setModalStatus(false);
+                  history.push("/");
+                }}
+                className="apply-success-button"
+                type="button"
+              >
+                Close
+              </button>
+            </>
+          ) : (
+            <>
+              <div className="project-detail-padding">
+                <div className="decline-applicant-message">
+                  Are you sure to delete this project ?
                 </div>
-              ) : (
-                <>
-                  <button
-                    onClick={() => deleteProject(projectId)}
-                    className="decline-applicant-button"
-                    type="button"
-                  >
-                    Yes
-                  </button>
-                  <button
-                    onClick={() => setModalStatus(false)}
-                    className="decline-applicant-button"
-                    type="button"
-                  >
-                    No
-                  </button>
-                </>
-              )}
-            </div>
-          </div>
-        )}
+              </div>
+              <div className="decline-applicant-buttons">
+                {loader ? (
+                  <div className="project-deletion-loader">
+                    <p>Loading</p>
+                    <CircularProgress size={15} />
+                  </div>
+                ) : (
+                  <>
+                    <button
+                      onClick={() => deleteProject(projectId)}
+                      className="decline-applicant-button"
+                      type="button"
+                    >
+                      Yes
+                    </button>
+                    <button
+                      onClick={() => setModalStatus(false)}
+                      className="decline-applicant-button"
+                      type="button"
+                    >
+                      No
+                    </button>
+                  </>
+                )}
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </>
   );
