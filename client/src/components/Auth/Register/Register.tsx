bcdb1e407874ce8f43ce2d3757737819232f7670
MaximeArn <maxime-arnould@outlook.fr>
2021-02-09 21:29:08 +0100

    create google auth button

--- client/src/components/Auth/Register/Register.tsx
+++ client/src/components/Auth/Register/Register.tsx
@@ -4,6 +4,7 @@ import googleIcon from "src/assets/icons/googleIcon.svg";
 import modalClickHandler from "../../../utils/modalClickHandler";
 import inputMapper from "../../../utils/inputMapper";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import LoginButton from "../Google/LoginBtn";
 import "../modal.scss";
 
 const Register = ({
@@ -23,9 +24,7 @@ const Register = ({
   return (
     <div
       className="modalContainer"
-      onMouseDown={({ target }) =>
-        modalClickHandler({ target, modal, closeModal })
-      }
+      onMouseDown={({ target }) => modalClickHandler({ target, modal, closeModal })}
     >
       <div className="modal" id="modal" ref={modal}>
         <form method="POST" onSubmit={handleSubmit}>
@@ -45,29 +44,30 @@ const Register = ({
               </button>
             )}
           </div>
-          <footer className="modal-footer">
-            <p>
-              Already have an Account ?
-              <a
-                className="auth-modal"
-                onClick={() => swapModal({ modal: "register", modal2: "login" })}
-              >
-                Sign In
-              </a>
-            </p>
+        </form>
+        <footer className="modal-footer">
+          <p>
+            Already have an Account ?
+            <a
+              className="auth-modal"
+              onClick={() => swapModal({ modal: "register", modal2: "login" })}
+            >
+              Sign In
+            </a>
+          </p>
 
-            <div className="form-separator">
-              <span>or</span>
-            </div>
+          <div className="form-separator">
+            <span>or</span>
+          </div>
 
-            <div className="social-media">
-              <button className="google-connect">
+          <div className="social-media">
+            <LoginButton />
+            {/* <button className="google-connect">
                 <img src={googleIcon} alt="google icon" />
                 <span>Connect with Google</span>
-              </button>
-            </div>
-          </footer>
-        </form>
+              </button> */}
+          </div>
+        </footer>
       </div>
     </div>
   );
