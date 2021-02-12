df062032ea48c2743a698a4099be5b12629e57ef
Krysyx <anthololz31@yahoo.fr>
2021-02-12 05:37:04 +0100

    re arranged request user info controller method

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -154,13 +154,17 @@ const authRouter = {
     const oAuth2AuthorizationUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.OAUTH2_REDIRECT_URI}&response_type=token&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile&state=${state}&include_granted_scopes=true`;
     return res.status(200).json(oAuth2AuthorizationUrl);
   },
-  requestUserInfos: async ({ body: { access_token, token_type } }, res, next) => {
-    axios
-      .get("https://www.googleapis.com/oauth2/v1/userinfo", {
+  requestUserInfos: async (
+    { body: { access_token, token_type, expires_in } },
+    res,
+    next
+  ) => {
+    const { email, name, picture } = await axios.get(
+      "https://www.googleapis.com/oauth2/v1/userinfo",
+      {
         headers: { Authorization: `${token_type} ${access_token}` },
-      })
-      .then(({ data }) => console.log(data))
-      .catch((error) => console.log(error.response));
+      }
+    );
   },
 };
 
