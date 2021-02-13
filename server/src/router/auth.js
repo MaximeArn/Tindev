84ef911ac02ce7ac999b68d9f6d105fad2e9323c
Krysyx <anthololz31@yahoo.fr>
2021-02-13 21:52:37 +0100

    renamed auth controller google methods

--- server/src/router/auth.js
+++ server/src/router/auth.js
@@ -8,13 +8,13 @@ const {
   sendNewLink,
   resetPassword,
   clearCookies,
-  googleAuth,
-  requestUserInfos,
+  authorize,
+  verify,
 } = require("../controllers/auth");
 
 const authRouterWrapper = (connectedUsers) => {
-  router.get("/google", googleAuth);
-  router.post("/google/request_user_infos", requestUserInfos);
+  router.get("/google", authorize);
+  router.post("/google/request_user_infos", verify);
   router.post("/login", login);
   router.post("/register", register);
   router.post("/reset_password", resetPassword);
