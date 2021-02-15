2483fb7eeab1d72b7605fff951db7f6b5b66198b
Krysyx <anthololz31@yahoo.fr>
2021-02-15 23:17:42 +0100

    created google login validator

--- server/src/utils/validators/googleLoginValidator.js
+++ server/src/utils/validators/googleLoginValidator.js
@@ -1,5 +1,30 @@
-module.exports = (user, next) => {
+const { User } = require("../../models");
+const UserError = require("../CustomError");
+module.exports = async (user, next) => {
   if (user.suspended.status) {
-    //TODO: user account suspension checks, inspire from loginvalidator with maybe a few changes
+    const { duration } = user.suspended;
+
+    if (!duration) {
+      throw new UserError("Your account has been suspended permanently", 403);
+    }
+
+    if (Date.now() > duration.getTime()) {
+      user.suspended = {
+        status: false,
+        duration: false,
+      };
+
+      const updated = await user.save();
+      return updated;
+    }
+
+    const remaining = Math.floor(Math.abs(duration - new Date()) / 36e5);
+
+    throw new UserError(
+      `Your account has been suspended for ${remaining} more ${
+        remaining <= 1 ? "hour" : "hours"
+      }`,
+      403
+    );
   }
 };
